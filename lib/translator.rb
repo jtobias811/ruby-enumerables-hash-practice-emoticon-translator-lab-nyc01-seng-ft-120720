9d66a0b157dc53493999f446cb6dd002b621b50c
Jacob Tobias <jtobias811@gmail.com>
2020-11-09 17:08:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -36,10 +36,11 @@ def get_english_meaning(path, emoticon)
   result = new_hash.keys.find do |key|
     new_hash[key][:japanese] == emoticon
   end
-  
+
   if result == nil
     "Sorry, that emoticon was not found"
   else
     new_hash[result][:japanese]
+    binding.pru
   end
 end
