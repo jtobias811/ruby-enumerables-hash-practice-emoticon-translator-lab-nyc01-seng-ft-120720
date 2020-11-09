22e0d3d2c45cd2a4da710f244ade780fe144d1d8
Jacob Tobias <jtobias811@gmail.com>
2020-11-09 15:23:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require 'yaml'
 require 'pry'
 
 def load_library(path)
-  emoticons = YAML.load_file(path).each do |meaning, value|
+  emoticons = YAML.load_file(path)
   emoticon_hash = {"get_meaning" => {}, "get_emoticon" => {}}
 
   emoticons.each do |meaning, value|
