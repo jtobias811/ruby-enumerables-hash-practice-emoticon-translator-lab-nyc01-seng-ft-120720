aac2f828f17151c72aba86e8879130f24cc6d364
Jacob Tobias <jtobias811@gmail.com>
2020-11-09 17:05:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,7 @@ def get_japanese_emoticon(path, emoticon)
   if japanese_emoticon == nil
     "Sorry, that emoticon was not found"
   else
-    new_hash[emoticon][:japanese]
+    new_hash[japanese_emoticon][:japanese]
   end
 end
 
